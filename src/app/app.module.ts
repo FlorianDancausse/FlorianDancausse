314db0c1481f67c5995279dacc6ffcd2768047d0
Dancausse <dancausseflorian@gmail.com>
2020-10-03 01:16:32 +0200

    feat: add detailled resume + refactor titles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,6 +18,7 @@ import { DetailledInfosComponent } from './components/detailled-infos/detailled-
     BrowserModule,
     BrowserAnimationsModule,
     MatTooltipModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
