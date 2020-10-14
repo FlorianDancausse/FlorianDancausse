696c776693284488992905fd6c3d43d675f497fe
Dancausse <floriandancausse.dev@gmail.com>
2020-10-14 22:55:25 +0200

    refactor: remove aside tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    this.changeCSSvhProperty();
+  }
+
+  private changeCSSvhProperty(): void {
+    const root: HTMLElement = document.querySelector(':root');
+    root.style.setProperty('--vh', `${window.innerHeight / 100}px`);
+    window.addEventListener('resize', () => {
+      root.style.setProperty('--vh', `${window.innerHeight / 100}px`);
+    });
+  }
+}
